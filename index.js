f846c6a41c04db9b7241ff5fcb0a10a8cc36b8b0
Diego Maeoka <diego.maeoka@infomentum.co.uk>
2021-09-16 11:56:03 +0100

    Create slack app

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const express = require("express");
-const app = express();
+const { App } = require('@slack/bolt');
+require('dotenv').config();
 
-app.get("/", (req, res) => {
-	res.send("hello world");
+// Initializes your app with your bot token and signing secret
+const app = new App({
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
+	token: process.env.SLACK_BOT_TOKEN,
+	socketMode:true,
+	appToken: process.env.APP_TOKEN
 });
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log("⚡️ Bolt app is running!");
+app.message("hey", async ({ message, command, say }) => {
+	try {
+		say(`Yaaay! <@${message.user}> that command works!`);
+	} catch (error) {
+		console.log("err")
+		console.error(error);
+	}
 });
+
+(async () => {
+	// Start the app
+	await app.start(process.env.PORT || 3000);
+	console.log('⚡️ Bolt app is running!');
+})();

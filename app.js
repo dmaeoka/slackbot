8f6d3b4eb1db2ffaadb36ef4901f09c7474ba6ef
Diego Maeoka <diego.maeoka@infomentum.co.uk>
2021-09-28 10:05:45 +0100

    Version 1

--- app.js
+++ app.js
@@ -1,207 +1,453 @@
-// const axios = require('axios')
-const { App } = require('@slack/bolt');
-require('dotenv').config();
+const { App, AwsLambdaReceiver } = require("@slack/bolt");
+const axios = require("axios");
+const { Employees, Sicknesses } = require("./src/breatheHR");
+const botURL = "https://hooks.slack.com/services/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";
+const awsLambdaReceiver = new AwsLambdaReceiver({
+	signingSecret: process.env.SLACK_SIGNING_SECRET
+});
 
-// Initializes your app with your bot token and signing secret
+// Initializes your app with your bot token and the AWS Lambda ready receiver
 const app = new App({
-	signingSecret: process.env.SLACK_SIGNING_SECRET,
 	token: process.env.SLACK_BOT_TOKEN,
-	socketMode:true,
-	appToken: process.env.APP_TOKEN
+	appToken: process.env.SLACK_APP_TOKEN,
+	receiver: awsLambdaReceiver,
+	processBeforeResponse: true,
 });
 
-const fs = require('fs');
-let raw = fs.readFileSync('db.json');
-let faqs = JSON.parse(raw);
-
-app.message("hey", async ({ message, command, say }) => {
-	try {
-		say(`Yaaay! <@${message.user}> that command works!`);
-	} catch (error) {
-		console.log("err")
-		console.error(error);
-	}
+app.shortcut("sickness", async ({ shortcut, body, ack, client }) => {
+	await ack();
+	const info = await client.users.info({
+		user: shortcut.user.id,
+	});
+	const today = (() => {
+		const d = new Date();
+		if (d.getHours() > 17) {
+			d.setDate(d.getDate() + 1);
+		}
+		return d.toISOString().substr(0, 10);
+	})();
+	await client.views.open({
+		trigger_id: body.trigger_id,
+		view: {
+			type: "modal",
+			callback_id: "sickness-modal",
+			submit: {
+				type: "plain_text",
+				text: "Submit",
+				emoji: true,
+			},
+			close: {
+				type: "plain_text",
+				text: "Cancel",
+				emoji: true,
+			},
+			title: {
+				type: "plain_text",
+				text: "Report sickness",
+				emoji: true,
+			},
+			blocks: [
+				{
+					type: "section",
+					text: {
+						type: "plain_text",
+						text: `:wave: Hey ${info.user.real_name} !\nWe're sorry to hear you are not feeling great today! We wish you a speedy recovery!`,
+						emoji: true,
+					},
+				},
+				{
+					type: "divider",
+				},
+				{
+					type: "input",
+					element: {
+						type: "datepicker",
+						initial_date: today,
+						placeholder: {
+							type: "plain_text",
+							text: "Select a date",
+							emoji: true,
+						},
+						action_id: "start_date-action",
+					},
+					label: {
+						type: "plain_text",
+						text: "Start date",
+						emoji: true,
+					},
+				},
+				{
+					type: "divider",
+				},
+				{
+					type: "input",
+					optional: true,
+					element: {
+						type: "datepicker",
+						initial_date: today,
+						placeholder: {
+							type: "plain_text",
+							text: "Select a date",
+							emoji: true,
+						},
+						action_id: "end_date-action",
+					},
+					label: {
+						type: "plain_text",
+						text: "End date",
+						emoji: true,
+					},
+				},
+				{
+					type: "input",
+					element: {
+						type: "static_select",
+						placeholder: {
+							type: "plain_text",
+							text: "Select an item",
+							emoji: true,
+						},
+						options: [
+							{
+								text: {
+									type: "plain_text",
+									text: "Cough, Cold, Flu",
+									emoji: true,
+								},
+								value: "364545",
+							},
+							{
+								text: {
+									type: "plain_text",
+									text: "Digestive / Stomach",
+									emoji: true,
+								},
+								value: "364544",
+							},
+							{
+								text: {
+									type: "plain_text",
+									text: "Headache / migraine",
+									emoji: true,
+								},
+								value: "364543",
+							},
+							{
+								text: {
+									type: "plain_text",
+									text: "Non-work related injury",
+									emoji: true,
+								},
+								value: "364548",
+							},
+							{
+								text: {
+									type: "plain_text",
+									text: "Other",
+									emoji: true,
+								},
+								value: "364549",
+							},
+							{
+								text: {
+									type: "plain_text",
+									text: "Psychiatric / Stress",
+									emoji: true,
+								},
+								value: "364546",
+							},
+							{
+								text: {
+									type: "plain_text",
+									text: "Work Related Injury",
+									emoji: true,
+								},
+								value: "364547",
+							},
+						],
+						action_id: "sickness_type-action",
+					},
+					label: {
+						type: "plain_text",
+						text: "Sickness type",
+						emoji: true,
+					},
+				},
+				{
+					type: "input",
+					optional: true,
+					element: {
+						type: "plain_text_input",
+						multiline: true,
+						action_id: "reason-action",
+					},
+					label: {
+						type: "plain_text",
+						text: "Additional details",
+						emoji: true,
+					},
+				},
+			],
+		},
+	});
 });
 
-app.command('/knowledge', async ({ command, ack, say }) => {
+app.view("sickness-modal", async ({ ack, body, context }) => {
 	try {
 		await ack();
-		let message = { blocks: [] };
-		faqs.data.map((faq) => {
-			message.blocks.push(
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*Question ❓*',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: faq.question,
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*Answer ✔️*',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: faq.answer,
-					},
-				}
-			);
-		});
-		say(message);
-	} catch (error) {
-		console.log('err');
-		console.error(error);
-	}
-});
-
-app.command('/ticket', async ({ ack, body, client}) => {
-	// Acknowledge the command request
-	await ack();
-
-	try {
-		// Call views.open with the built-in client
-		const result = await client.views.open({
-			// Pass a valid trigger_id within 3 seconds of receiving it
-			trigger_id: body.trigger_id,
-			// View payload
-			view: {
-				type: 'modal',
-				// View identifier
-				callback_id: 'view_1',
-				title: {
-					type: 'plain_text',
-					text: 'Modal title'
-				},
-				blocks: [{
-						type: 'section',
-						text: {
-							type: 'mrkdwn',
-							text: 'Welcome to a modal with _blocks_'
-						},
-						accessory: {
-							type: 'button',
-							text: {
-								type: 'plain_text',
-								text: 'Click me!'
-							},
-							action_id: 'button_abc'
-						}
-					},
-					{
-						type: 'input',
-						block_id: 'input_c',
-						label: {
-							type: 'plain_text',
-							text: 'What are your hopes and dreams?'
-						},
-						element: {
-							type: 'plain_text_input',
-							action_id: 'dreamy_input',
-							multiline: true
-						}
-					}
-				],
-				submit: {
-					type: 'plain_text',
-					text: 'Submit'
+		const { values } = body.view.state;
+		const answer = Object.values(values);
+		const users = await new Employees().get();
+		const currentUser = users.filter((item) => item.email == "diego@maeoka.dev")[0];
+		const currentEmployee = new Employees(currentUser.id);
+		const start_date = answer[0]["start_date-action"]["selected_date"].split("-").reverse().join("/");
+		const end_date = (() => {
+			let date_1 = new Date(answer[0]["start_date-action"]["selected_date"]);
+			let date_2 = answer[1]["end_date-action"]["selected_date"];
+			if (date_2) {
+				date_2 = new Date(date_2);
+				if (date_2.getTime() - date_1.getTime() > 0) {
+					return date_2.toLocaleDateString();
+				} else {
+					return date_1.toLocaleDateString();
 				}
 			}
+			return null;
+		})();
+		const sicknesstype_id = answer[2]["sickness_type-action"]["selected_option"].value;
+		const reason = answer[3]["reason-action"].value;
+		const params = {
+			start_date: start_date,
+			half_start_am_pm: "am",
+			end_date: end_date,
+			company_sicknesstype_id: sicknesstype_id,
+			reason: reason,
+		};
+		let text;
+		try {
+			const results = await currentEmployee.createSickness(params);
+			text = "Wish you a speedy recovery! See you soon!";
+		} catch (error) {
+			text = "Something went wrong with the BreatheHR API, please check try on the website ";
+		}
+
+		axios({
+			url: botURL,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: {
+				text: text
+			},
 		});
-		console.log(result);
 	} catch (error) {
-		console.error(error);
+		console.warn(error);
 	}
 });
 
-app.message(/products/, async ({ command, say }) => {
-	try {
-		let message = { blocks: [] };
-		const productsFAQs = faqs.data.filter(
-			(faq) => faq.keyword === 'products'
-		);
+app.shortcut("leave", async ({ shortcut, body, ack, client }) => {
+	await ack();
+	const info = await client.users.info({
+		user: shortcut.user.id,
+	});
+	const today = (() => {
+		const d = new Date();
+		if (d.getHours() > 17) {
+			d.setDate(d.getDate() + 1);
+		}
+		return d.toISOString().substr(0, 10);
+	})();
 
-		productsFAQs.map((faq) => {
-			message.blocks.push(
+	await client.views.open({
+		trigger_id: body.trigger_id,
+		view: {
+			type: "modal",
+			callback_id: "leave-modal",
+			submit: {
+				type: "plain_text",
+				text: "Submit",
+				emoji: true,
+			},
+			close: {
+				type: "plain_text",
+				text: "Cancel",
+				emoji: true,
+			},
+			title: {
+				type: "plain_text",
+				text: "Add leave request",
+				emoji: true,
+			},
+			blocks: [
 				{
-					type: 'section',
+					type: "section",
 					text: {
-						type: 'mrkdwn',
-						text: '*Question ❓*',
+						type: "plain_text",
+						text: `Hey ${info.user.real_name}! Are you going on holidays?`,
+						emoji: true,
 					},
 				},
 				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: faq.question,
+					type: "divider",
+				},
+				{
+					type: "input",
+					element: {
+						type: "datepicker",
+						initial_date: today,
+						placeholder: {
+							type: "plain_text",
+							text: "Select a date",
+							emoji: true,
+						},
+						action_id: "start_date-action",
+					},
+					label: {
+						type: "plain_text",
+						text: "Start Date",
+						emoji: true,
 					},
 				},
 				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*Answer ✔️*',
+					type: "divider",
+				},
+				{
+					type: "input",
+					optional: true,
+					element: {
+						type: "datepicker",
+						initial_date: today,
+						placeholder: {
+							type: "plain_text",
+							text: "Select a date",
+							emoji: true,
+						},
+						action_id: "end_date-action",
+					},
+					label: {
+						type: "plain_text",
+						text: "End date",
+						emoji: true,
 					},
 				},
 				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: faq.answer,
+					type: "context",
+					elements: [
+						{
+							type: "plain_text",
+							text: "You can leave the end date blank if taking a day",
+						},
+					],
+				},
+				{
+					type: "input",
+					optional: true,
+					element: {
+						type: "plain_text_input",
+						multiline: true,
+						action_id: "notes-action",
 					},
-				}
-			);
-		});
-
-		say(message);
-	} catch (error) {
-		console.log('err');
-		console.error(error);
-	}
+					label: {
+						type: "plain_text",
+						text: "Notes",
+						emoji: true,
+					},
+				},
+			],
+		},
+	});
 });
 
-app.command('/update', async ({ command, ack, say }) => {
+app.view("leave-modal", async ({ ack, body, context }) => {
 	try {
 		await ack();
-		const data = command.text.split('|');
-
-		const newFAQ = {
-			keyword: data[0].trim(),
-			question: data[1].trim(),
-			answer: data[2].trim(),
+		const { values } = body.view.state;
+		const answer = Object.values(values);
+		const users = await new Employees().get();
+		const currentUser = users.filter((item) => item.email == "diego@maeoka.dev")[0];
+		const currentEmployee = new Employees(currentUser.id);
+		const start_date = answer[0]["start_date-action"]["selected_date"].split("-").reverse().join("/");
+		const end_date = (() => {
+			let date_1 = new Date(answer[0]["start_date-action"]["selected_date"]);
+			let date_2 = answer[1]["end_date-action"]["selected_date"];
+			if (date_2) {
+				date_2 = new Date(date_2);
+				if (date_2.getTime() - date_1.getTime() > 0) {
+					return date_2.toLocaleDateString();
+				}
+			}
+			return start_date;
+		})();
+		const params = {
+			start_date: start_date,
+			end_date: end_date,
+			half_start_am_pm: null,
+			half_end_am_pm: null,
+			type: "Holiday",
+			notes: answer[2]["notes-action"].value,
 		};
+		let text;
+		try {
+			const results = await currentEmployee.createLeaveRequests(params);
+			text = `You have sucesfully booked a leave from ${start_date} to ${end_date}.`;
+		} catch (error) {
+			text = "Booking non authorized via API, please check on BreatheHR";
+		}
 
-		// save data to db.json
-		fs.readFile('db.json', function (err, data) {
-			const json = JSON.parse(data);
-			json.data.push(newFAQ);
-			fs.writeFile('db.json', JSON.stringify(json), function (err) {
-				if (err) throw err;
-				console.log('Successfully saved to db.json!');
-			});
+		axios({
+			url: botURL,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: {
+				text: text,
+			},
 		});
-
-		say(`You've added a new FAQ with the keyword *${newFAQ.keyword}.*`);
+		return null;
 	} catch (error) {
-		console.log('err');
-		console.error(error);
+		console.warn(error);
 	}
 });
 
-(async () => {
-	// Start the app
-	await app.start(process.env.PORT || 3000);
-	console.log('⚡️ Bolt app is running!');
-})();
+app.event("app_home_opened", async ({ event, client }) => {
+	await client.views.publish({
+		user_id: event.user,
+		view: {
+			type: "home",
+			blocks: [
+				{
+					type: "section",
+					block_id: "section678",
+					text: {
+						type: "mrkdwn",
+						text: "App Home Published",
+					},
+				},
+			],
+		},
+	});
+});
+
+// Handle the Lambda function event
+module.exports.slack = async (event, context, callback) => {
+	const handler = await awsLambdaReceiver.start();
+	return handler(event, context, callback);
+};
+
+module.exports.home = async (event) => {
+
+	const pacientes = [
+		{ id: 1, nome: "Maria", dataNascimento: "1984-11-01" },
+		{ id: 2, nome: "Joao", dataNascimento: "1980-01-16" },
+		{ id: 3, nome: "Jose", dataNascimento: "1998-06-06" },
+	];
+	return {
+		statusCode: 200,
+		// body: JSON.stringify({pacientes}, null, 2),
+		body: JSON.stringify({
+			SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN
+		})
+	};
+};
---
+++ src/breatheHR.js
@@ -0,0 +1,196 @@
+const axios = require("axios");
+const objectToQueryString = require("./uri");
+
+class Breathe {
+	constructor() {
+		this.token = process.env.BREATHE_TOKEN;
+	}
+
+	urlParams(parameters = {}) {
+		if (Object.values(parameters).length > 0) {
+			return "?" + objectToQueryString(parameters);
+		}
+		return;
+	}
+
+	execute(method = "GET", endpoint, parameters = "") {
+		try {
+			// const url = `https://api.sandbox.breathehr.info/v1/${endpoint}${parameters}`;
+			const url = `https://api.breathehr.com/v1/${endpoint}${parameters}`;
+			return axios({
+				url,
+				method,
+				headers: {
+					"X-API-KEY": this.token,
+					"Content-Type": "application/json",
+				},
+			});
+		} catch (error) {
+			console.warn(error);
+		}
+	}
+
+	async get(page = 1, per_page = 100) {
+		const query_string = this.urlParams({
+			page,
+			per_page,
+		});
+		const response = await this.execute("GET", this.endpoint, query_string);
+		return response.data[this.endpoint] || [];
+	}
+}
+
+class Employees extends Breathe {
+	constructor(id) {
+		super();
+		this.endpoint = "employees";
+		this.id = id;
+	}
+
+	async get(page = 1, per_page = 100, filter = null) {
+		const query_string = this.urlParams({
+			page,
+			per_page,
+		});
+		let response;
+		if (this.id) {
+			response = await this.execute("GET", this.endpoint, `/${this.id}`);
+			return response.data[this.endpoint][0] || [];
+		}
+
+		response = await this.execute("GET", this.endpoint, query_string);
+		return response.data[this.endpoint] || [];
+	}
+
+	async leaveRequests(
+		exclude_cancelled_absences = true,
+		page = 1,
+		per_page = 100
+	) {
+		const query_string = this.urlParams({
+			exclude_cancelled_absences,
+			page,
+			per_page,
+		});
+		const response = await this.execute(
+			"GET",
+			this.endpoint,
+			`/${this.id}/leave_requests${query_string}`
+		);
+		return response.data["leave_requests"] || [];
+	}
+
+	async createLeaveRequests(params = {}) {
+		const query_string = this.urlParams({
+			id: this.id,
+			leave_request: params,
+		});
+		const response = await this.execute(
+			"POST",
+			this.endpoint,
+			`/${this.id}/leave_requests${query_string}`
+		);
+		return response.data["leave_requests"] || [];
+	}
+
+	async sicknesses() {
+		const query_string = this.urlParams({
+			id: this.id,
+		});
+		const response = await this.execute(
+			"GET",
+			this.endpoint,
+			`/${this.id}/sicknesses${query_string}`
+		);
+		return response.data["sicknesses"] || [];
+	}
+
+	async createSickness(params = {}) {
+		const query_string = this.urlParams({
+			id: this.id,
+			sickness: params,
+		});
+		const response = await this.execute(
+			"POST",
+			this.endpoint,
+			`/${this.id}/sicknesses${query_string}`
+		);
+		return response.data["sicknesses"] || [];
+	}
+}
+
+class EmployeeTrainingCourses extends Breathe {
+	constructor() {
+		super();
+		this.endpoint = "employee_training_courses";
+	}
+}
+
+class HolidayAllowances extends Breathe {
+	constructor() {
+		super();
+		this.endpoint = "holiday_allowances";
+	}
+}
+
+class LeaveRequests extends Breathe {
+	constructor(id) {
+		super();
+		this.endpoint = "leave_requests";
+		this.id = id;
+	}
+
+	async get(page = 1, per_page = 100, parameters = {}) {
+		const defaultParameters = Object.assign(parameters, {
+			page,
+			per_page,
+		});
+		const query_string = this.urlParams(defaultParameters);
+		const response = await this.execute("GET", this.endpoint, query_string);
+		return response.data[this.endpoint] || [];
+	}
+
+	async cancelling() {
+		const response = await this.execute(
+			"GET",
+			this.endpoint,
+			`/${this.id}/cancelling`
+		);
+		return response.data[this.endpoint] || [];
+	}
+
+	async otherLeaveReasons() {
+		const response = await this.execute(
+			"GET",
+			this.endpoint,
+			`/${this.id}/other_leave_reasons`
+		);
+		return response.data["other_leave_reasons"] || [];
+	}
+}
+
+class Locations extends Breathe {
+	constructor() {
+		super();
+		this.endpoint = "locations";
+	}
+}
+
+class OtherLeaveReasons extends Breathe {
+	constructor() {
+		super();
+		this.endpoint = "other_leave_reasons";
+	}
+}
+
+class Sicknesses extends Breathe {
+	constructor(id) {
+		super();
+		this.endpoint = "sicknesses";
+	}
+}
+
+module.exports = {
+	Employees,
+	Sicknesses,
+};
